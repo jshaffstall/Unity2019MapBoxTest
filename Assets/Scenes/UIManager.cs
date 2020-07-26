ffb70cd9de9dd382a3a1eed4c07a988b2f9d989d
jshaffstall <jshaffstall@gmail.com>
2020-07-26 17:10:03 -0400

    Done with video 6

---
+++ Assets/Scenes/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text xpText;
+    [SerializeField] private Text levelText;
+    [SerializeField] private GameObject menu;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(xpText);
+        Assert.IsNotNull(levelText);
+        Assert.IsNotNull(menu);
+    }
+
+    public void updateLevel(int level)
+    {
+        levelText.text = level.ToString();
+    }
+
+    public void updateXP(int current, int required)
+    {
+        xpText.text = current.ToString() + " / " + required.ToString();
+    }
+
+    public void toggleMenu()
+    {
+        menu.SetActive(!menu.activeSelf);
+    }
+}
