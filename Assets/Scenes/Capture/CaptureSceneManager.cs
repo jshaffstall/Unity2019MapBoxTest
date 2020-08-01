3aeb9ffb63356cb62aa17d454406ee83d42269c5
jshaffstall <jshaffstall@gmail.com>
2020-08-01 15:43:55 -0400

    Video 17

---
+++ Assets/Scenes/Capture/CaptureSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureSceneManager : PocketDroidsSceneManager
+{
+    public override void playerTapped(GameObject player)
+    {
+        print("playerTapped");
+    }
+
+    public override void droidTapped(GameObject droid)
+    {
+        print("droidTapped");
+    }
+}
